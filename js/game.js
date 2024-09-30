9bcf5ab6285cdeb32ed6baa653a3ecc6e7de86ab
anasamhs2017 <anasox.amh@gmail.com>
2024-09-30 16:58:31 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,3 +1,26 @@
+
+function depositTON(telegramId, amount) {
+    fetch('https://anasamhs2017.github.io/deposit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ telegramId, amount }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+// Show QR Code
+function showDepositQRCode(walletAddress) {
+    $('#qrcode').empty();
+    $('#qrcode').qrcode(walletAddress);
+}
 function submitReferral(referralCode, telegramId) {
     fetch('https://anasamhs2017.github.io/submit-referral', { // Update this URL to your server
         method: 'POST',
