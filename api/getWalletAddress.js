465f3b28aff959dfd644e9593c38fff81f0180ec
anasamhs2017 <anasox.amh@gmail.com>
2024-10-04 15:03:19 +0100

    Update getWalletAddress.js

--- api/getWalletAddress.js
+++ api/getWalletAddress.js
@@ -1,33 +1,3 @@
-// api/index.js
-const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
-
-const token = '8047643548:AAH9GroXRXG3bD8OGa6rBN-grgIGk1pM6LM';
-const bot = new TelegramBot(token);
-
-export default async (req, res) => {
-    if (req.method === 'POST') {
-        const update = req.body;
-
-        // Handle incoming updates from Telegram
-        if (update.message) {
-            const chatId = update.message.chat.id;
-
-            // Example command handling
-            if (update.message.text === '/start') {
-                bot.sendMessage(chatId, 'Welcome! Type /openapp to access the mini app.');
-            }
-
-            // Other command handling can go here...
-        }
-
-        res.status(200).send('OK');
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
-};
-
-    
 // pages/api/getWalletAddress.js
 const addresses = [
     "EQB4djqmv2I3jFdfw12jp9iGuC4uXtGpX1GaZ-5W5_EOsb3K",
